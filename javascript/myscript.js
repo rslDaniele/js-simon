075fabdb86860a35eb5fac0cb8c380c94fb3a83b
Daniele <danlup94@gmail.com>
2020-11-05 16:30:34 +0100

    Esercizio completato

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,60 +1,63 @@
-setTimeout(chiediNum, 3000);
+$(document).ready(function () {
 
-alert("Benvenuto giocatore! Pronto a misurare la tua memoria?")
+  alert("Benvenuto giocatore! Pronto a misurare la tua memoria?")
 
-var numeri = [];
+  var numeri = [];
 
-var numeriUser = [];
+  var numeriUser = [];
 
-var punteggio = 0;
+  var punteggio = 0;
 
-// ALERT VISUALIZZA 5 NUMERI
+  // ALERT VISUALIZZA 5 NUMERI
 
-function generaNum() {
+  function generaNum() {
 
-  for (var i = 0; i < 5; i++) {
-    var ricorda = Math.floor(Math.random()* 100 + 1);
-    if (!numeri.includes(ricorda)) {
-      numeri.push(ricorda);
-      console.log(ricorda);
+    while (numeri.length < 5) {
+
+      var ricorda = Math.floor(Math.random()* 100 + 1);
+      if (!numeri.includes(ricorda)) {
+        numeri.push(ricorda);
+        console.log(ricorda);
+      }
     }
   }
-}
 
-generaNum(),
-console.log(numeri);
+  generaNum(),
+  console.log(numeri);
 
-alert("Concentrati! " + numeri);
+  alert("Concentrati! " + numeri);
 
+  setTimeout(chiediNum, 10000);
 
-// DOPO 30 SECONDI VIENE CHIESTO AD UTENTE DI INSERIRE 5 NUMERI
+  // IL SISTEMA CHIEDE AL GIOCATORE DI RICORDARE I NUMERI
 
-// IL SISTEMA COMUNICA QUANTI NUMERI SONO STATI RICORDATI
+  function chiediNum() {
 
-function chiediNum() {
+    while (numeriUser.length < numeri.length) {
+      var guess = parseInt(prompt("Inserisci un numero!"));
+      console.log(guess);
+      if (!numeriUser.includes(guess)) {
+        numeriUser.push(guess);
+        console.log(numeriUser);
+      }else {
+        alert("Sicuro? Non mi sembra ci fossero doppioni...")
+      }
 
-  for (var i = 0; i < numeri.length; i++) {
-    var guess = parseInt(prompt("Inserisci un numero!"));
-    console.log(guess);
-    if (!numeriUser.includes(guess)) {
-      numeriUser.push(guess);
-      console.log(numeriUser);
-    }else {
-      alert("Sicuro? Non mi sembra ci fossero doppioni...")
+      if (numeri.includes(guess)) {
+        punteggio++;
+
+      }
+
     }
-
-    if (numeri.includes(guess)) {
-      punteggio++;
-
+    // IL SISTEMA COMUNICA IL PUNTEGGIO AL GIOCATOREs
+    if (punteggio <= 2) {
+      document.getElementById('punti').innerHTML = ("Hmmm " + punteggio + " punti, dovremo lavorarci su...");
+    } else if (punteggio <= 4) {
+      document.getElementById('punti').innerHTML = ("Bella prova! Hai totalizzato " + punteggio + " punti!");
+    } else {
+      document.getElementById('punti').innerHTML = ("Eccezionale! Hai totalizzato " + punteggio + " punti su 5!");
     }
 
   }
-  if (punteggio <= 2) {
-    console.log("Hmmm " + punteggio + " punti, dovremo lavorarci su...");
-  } else if (punteggio <= 4) {
-    console.log("Bella prova! Hai totalizzato " + punteggio + " punti!");
-  } else {
-    console.log("Eccezionale! Hai totalizzato " + punteggio + " punti su 5!");
-  }
 
-}
+})
