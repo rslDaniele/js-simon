110cc55dbf6fa1f73559ee998193920d2ae4b4fa
Daniele <danlup94@gmail.com>
2020-11-05 21:22:27 +0100

    Esercizio completato
    refactoring

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -15,18 +15,21 @@ $(document).ready(function () {
     while (numeri.length < 5) {
 
       var ricorda = Math.floor(Math.random()* 100 + 1);
+
       if (!numeri.includes(ricorda)) {
         numeri.push(ricorda);
-        console.log(ricorda);
       }
+
     }
+    console.log(numeri);
+
     document.getElementById('memory').innerHTML = ("Memorizza questi numeri, hai 5 secondi! " + numeri)
   }
 
   // AL CLICK DEL PULSANTE IL GIOCO GENERA ARRAY NUMERI
 
   document.getElementById('play').addEventListener('click', generaNum);
-  console.log(numeri);
+
 
   // IL DIV CHE MOSTRA I NUMERI GENERATI SPARISCE
 
@@ -41,10 +44,10 @@ $(document).ready(function () {
 
     while (numeriUser.length < numeri.length) {
       var guess = parseInt(prompt("Inserisci un numero!"));
-      console.log(guess);
+
       if (!numeriUser.includes(guess)) {
         numeriUser.push(guess);
-        console.log(numeriUser);
+
       }else {
         alert("Sicuro? Non mi sembra ci fossero doppioni...")
       }
@@ -55,11 +58,15 @@ $(document).ready(function () {
       }
 
     }
+    console.log(numeriUser);
+
     // IL SISTEMA COMUNICA IL PUNTEGGIO AL GIOCATOREs
     if (punteggio <= 2) {
       document.getElementById('punti').innerHTML = ("Hmmm " + punteggio + " punti, dovremo lavorarci su...");
+
     } else if (punteggio <= 4) {
       document.getElementById('punti').innerHTML = ("Bella prova! Hai totalizzato " + punteggio + " punti!");
+
     } else {
       document.getElementById('punti').innerHTML = ("Eccezionale! Hai totalizzato " + punteggio + " punti su 5!");
     }
