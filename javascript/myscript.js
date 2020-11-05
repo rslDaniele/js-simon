b6151061eaf131f7ca0cb63b5cfe5c767d4fcf90
Daniele <danlup94@gmail.com>
2020-11-05 15:47:37 +0100

    Correzione codice js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,10 @@
+setTimeout(chiediNum, 30000);
+
 alert("Benvenuto giocatore! Pronto a misurare la tua memoria?")
 
 var numeri = [];
+
+var numeriUser = [];
 
 var punteggio = 0;
 
@@ -10,9 +14,9 @@ function generaNum() {
 
   for (var i = 0; i < 5; i++) {
     var ricorda = Math.floor(Math.random()* 100 + 1);
-    console.log(ricorda);
     if (!numeri.includes(ricorda)) {
       numeri.push(ricorda);
+      console.log(ricorda);
     }
   }
 }
@@ -25,20 +29,23 @@ alert("Concentrati! " + numeri);
 
 // DOPO 30 SECONDI VIENE CHIESTO AD UTENTE DI INSERIRE 5 NUMERI
 
+// IL SISTEMA COMUNICA QUANTI NUMERI SONO STATI RICORDATI
+
 function chiediNum() {
 
   for (var i = 0; i < numeri.length; i++) {
     var guess = parseInt(prompt("Inserisci un numero!"));
+    console.log(guess);
+    if (!numeriUser.includes(guess)) {
+      numeriUser.push(guess);
+      console.log(numeriUser);
+    }else {
+      alert("Sicuro? Non mi sembra ci fossero doppioni...")
+    }
+
     if (numeri.includes(guess)) {
       punteggio++;
+      console.log("Complimenti! Hai ottenuto " + punteggio + " punti!")
     }
   }
 }
-
-setTimeout(chiediNum, 30000);
-
-
-
-// IL SISTEMA COMUNICA QUANTI NUMERI SONO STATI RICORDATI
-
-console.log(punteggio);
